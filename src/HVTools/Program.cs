18754534337bb6eeaba4c9c0ebaf4931d5858367
Michael Morten Sonne <49366751+michaelmsonne@users.noreply.github.com>
2026-02-08 19:18:20 +0100

    Use null-forgiving on ApplicationExit handler
    Add the null-forgiving operator (!) when subscribing OnApplicationExit to Application.ApplicationExit to suppress C# nullable-reference compiler warnings, indicating the handler is intentionally non-null and avoiding spurious diagnostics.

--- src/HVTools/Program.cs
+++ src/HVTools/Program.cs
@@ -16,7 +16,7 @@ namespace HVTools
             FileLogger.WriteStartupBanner();
 
             // Register application exit event to write shutdown banner
-            Application.ApplicationExit += OnApplicationExit;
+            Application.ApplicationExit += OnApplicationExit!;
 
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
